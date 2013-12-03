620ea9a5dd1477cf8564dd06bed8b6ca6d7cfbd9
yaminisudha <sudha250990@gmail.com>
2013-12-03 14:42:53 +0530

    Insert into inventoryvoucher n inventoryvoucherlist successful

--- api/stockjournal.php
+++ api/stockjournal.php
@@ -25,28 +25,7 @@ $app->get('/stockitemlist', function () use ($app) {
 $app->post('/addjournalentry', function () use ($app) {
 	$request = (array) json_decode($app->request()->getBody());
 	
-	
-try{
-	$data=$request[0];
-	
 	$vouchertype=1;
-	$name=$data->result[0]->name;
-	$qty=$data->result[0]->qty;
-	$units=$data->result[0]->units;
-	$rate=$data->result[0]->rate;
-	$amt=$data->result[0]->amt;
-	
-	$stockitemid=1;
-	
-}
-catch (Exception $e) 
-	{
-	logerrors::writelog('addstockjournalentry','api/stockjournal.php',$e->getMessage());
-	$app->response()->header('Content-Type', 'application/json');
-	echo json_encode('-3');
-	return;
-	}
-	
 	
 try{
 	
@@ -54,6 +33,7 @@ $sql="INSERT INTO tbl_inventoryvoucher(vouchertype,createdby,createdon,modifiedb
 modifiedon) VALUES(:vouchertype,:createdby,CURRENT_TIMESTAMP(),:modifiedby,CURRENT_TIMESTAMP())";
 
 	$conn = getConnection();
+	//$conn->beginTransaction ();
 	$stmt = $conn->prepare($sql);
 
 	$stmt->bindParam('vouchertype',$vouchertype);
@@ -62,48 +42,64 @@ modifiedon) VALUES(:vouchertype,:createdby,CURRENT_TIMESTAMP(),:modifiedby,CURRE
 	$stmt->execute();
 
 	if($stmt->errorCode() == 0) 
-		{
+	{
 		$insert_id = $conn->lastInsertId();
 		try{
-	
-		$sql2="INSERT INTO tbl_inventoryvoucherlist(inventoryvoucherid,inventory,quantity,rate,createdby,createdon,modifiedby,
-		modifiedon) VALUES(:vouchertype,:inventory,:quantity,:rate,:createdby,CURRENT_TIMESTAMP(),:modifiedby,CURRENT_TIMESTAMP())";
+					
+			$data=$request[0];
+			$sizeofdata=sizeof($data->result);
+				
+			for ($i=0; $i < $sizeofdata; $i++) { 
 		
-			$conn2 = getConnection();
-			$stmt2 = $conn2->prepare($sql2);
-		
-			$stmt2->bindParam('inventoryvoucherid',$insert_id);
-			$stmt2->bindParam('inventory',$stockitemid);
-			$stmt2->bindParam('quantity',$qty);
-			$stmt2->bindParam('rate',$rate);
-			$stmt2->bindParam('createdby',$_SESSION['ca_loginuserid']);
-			$stmt2->bindParam('modifiedby',$_SESSION['ca_loginuserid']);
-			$stmt2->execute();
-		
-			if($stmt2->errorCode() == 0) 
-				{
-				$insert_id1 = $conn2->lastInsertId();
-				$app->response()->header('Content-Type', 'application/json');
-				echo json_encode($insert_id);
-				return;
-				} 
+				$name=$data->result[$i]->name;
+				$qty=$data->result[$i]->qty;
+				$units=$data->result[$i]->units;
+				$rate=$data->result[$i]->rate;
+				$amt=$data->result[$i]->amt;
+			
+				$sql="INSERT INTO tbl_inventoryvoucherlist(inventoryvoucherid,inventory,quantity,rate,createdby,createdon,modifiedby,
+				modifiedon) VALUES(:inventoryvoucherid,(select id from tbl_stockitem where name=:name),:quantity,:rate,:createdby,CURRENT_TIMESTAMP(),:modifiedby,CURRENT_TIMESTAMP())";
+				
+				//$conn2 = getConnection();
+				$stmt = $conn->prepare($sql);
+			
+				$stmt->bindParam('inventoryvoucherid',$insert_id);
+				$stmt->bindParam('name',$name);
+				$stmt->bindParam('quantity',$qty);
+				$stmt->bindParam('rate',$rate);
+				$stmt->bindParam('createdby',$_SESSION['ca_loginuserid']);
+				$stmt->bindParam('modifiedby',$_SESSION['ca_loginuserid']);
+				$stmt->execute();		 
 			}
-			catch (PDOException $e) 
+			
+			if($stmt->errorCode() == 0) 
 			{
+				$lastinsertid = $conn->lastInsertId();
+				$app->response()->header('Content-Type', 'application/json');
+				echo json_encode($lastinsertid);
+				return;
+			}
+		}
+		catch (PDOException $e) 
+		{
 			logerrors::writelog('addstockjournalentry','api/stockjournal.php',$e->getMessage());
 			$app->response()->header('Content-Type', 'application/json');
 			echo json_encode('-1');
 			return;
-			}				
-		} 
+		}				
 	}
-	catch (PDOException $e) 
-	{
+	//$conn->commit(); 
+}
+catch (PDOException $e) 
+{
+	//if(isset($conn))
+	//	$conn->rollback();
+	
 	logerrors::writelog('addstockjournalentry','api/stockjournal.php',$e->getMessage());
 	$app->response()->header('Content-Type', 'application/json');
 	echo json_encode('-2');
 	return;
-	}
+}
 	
 });
 
