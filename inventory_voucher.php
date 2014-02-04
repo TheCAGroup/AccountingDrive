93ab0cb041265907d94e2ff6708277359224eeec
yamini <yamini@localhost.localdomain>
2014-02-04 05:50:48 +0000

    inventory voucher - Look n feel changed

--- inventory_voucher.php
+++ inventory_voucher.php
@@ -27,13 +27,13 @@
 				width:5%;
 				min-width:5%;
 			}
-			.tab_col_3{
-				width:3%;
-				min-width:3%;
+			.tab_col_4{
+				width:4%;
+				min-width:4%;
 			}
 		</style>
 	</head>
-	<body>
+        <body style="height:100%;">
 		
 		
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
@@ -41,77 +41,43 @@
 		<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 		<script type="text/javascript" src="bootstrap/js/bootstrap-modal.js"></script>
 		<script type="text/javascript" src="js/config.js"></script>
+                
 		<?php
 /*include 'breadcrumb.php';
 makemodal_confirm("modalacceptchange","Accept","");
 echo $callbackvalue;*/
 ?>
 		<div class="container">
-		<div class="row show-grid">
-  		<div class="col-lg-10">
-		<div class="well">
+		<div class="row show-grid" style="height: 96%;">
+  		<div class="col-lg-10" style="height: 100%;">
+		<div class="well" style="height: 100%;">
 		<h4 align="center">Transfer of Materials</h4>
-		
-		<!--div class="my-class" style=" border: 1px #0D3349 solid; background: #EFF8FB;  width: 100%; height: 35%;">
-			<table width="100%" height="100%" border="1">
-				<tr>
-					<td height="5%" colspan="7"><center><b>Source(Consumption)</b></center></td>
-				</tr>
-				
-				<tr style="font-size: 0.875em;border: 0px;">
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_5"></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_50"><b>Name of Item</b></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_7"><b>Qty</b></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"><b>Units</b></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_7"><b>Rate</b></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"><b>Amount</b></td>
-					<td height="5%" style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"></td>
-				</tr>
-				
-				<tr>
-					<td height="70%" colspan="7">
-					<div style="overflow: auto;height:'70%'">
-						<table id="mysource" class="table" style="border: 0px">
-							
-						</table>
-					</div>
-					</td>
-				</tr>
-				
-				<tr>
-					<td height="10%"><a href="#" onclick="addRow('mysource')" class="btn btn-default-sm"> + Add Row</a></td>
-					<td height="10%"><a href="#" onclick="deleteSelectedRows('mysource')" class="btn btn-default-sm"> X Delete Rows</a></td>
-					<td height="10%"></td>
-					<td height="10%"></td>
-					<td height="10%"></td>
-					<td height="10%"></td>
-					<td height="10%"><a href="#" onclick="endList('mysource')" class="btn btn-default-sm"> End List</a></td>	
-					
-				</tr>
-			</table>
-		</div-->
+                <br>
 		
 		
-		<div class="my-class" style=" border: 1px #0D3349 solid; background: #EFF8FB;  width: 100%; height: 35%;">
+		<div style=" width: 100%; height: 35%;">
 			
-			<div style="width: 100%;float:left; height:20%">
-				<b><center>Source (Consumption)</center></b>
+			<div style="width: 100%;float:left; height:20%;">
+                            
 				<table style="width:100%;">
 					<thead>
-						<tr style="font-size: 0.875em;border: 0px">
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_5"></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_50"><b>Name of Item</b></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_7"><b>Qty</b></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"><b>Units</b></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_7"><b>Rate</b></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"><b>Amount</b></td>
-							<td style="border-bottom: 1px #0D3349 dotted;" class="tab_col_10"></td>
-						</tr>
+                                            <tr style="font-size: 0.875em;background-color: #E9F2F5;">
+                                                <td colspan="7" align="center"><b>Source (Consumption)</b></td>
+                                            </tr>
+                                            <tr style="font-size: 0.875em;border: 0px;background-color: #C8DBE2;">
+                                                    <td class="tab_col_5"></td>
+                                                    <td class="tab_col_50"><b>Name of Item</b></td>
+                                                    <td class="tab_col_7"><b>Qty</b></td>
+                                                    <td class="tab_col_10"><b>Units</b></td>
+                                                    <td class="tab_col_7"><b>Rate</b></td>
+                                                    <td class="tab_col_10"><b>Amount</b></td>
+                                                    <td class="tab_col_10"></td>
+                                            </tr>
 					</thead>
 				</table>
 			</div>
 			
-			<div style="overflow: auto;float:left;height:60%; width:100%; margin-top: 10px;">
+			<div style="overflow: none;float:left; width:100%; margin-top: 10px;">
 				<table id="mysource" class="table" style="border: 0px">
 					
 				</table>
@@ -148,23 +114,63 @@ echo $callbackvalue;*/
                         </div>  
 		</div><br>
 		
-		<div class="my-class" align="center" style=" border: 1px #0D3349 solid; background: #EFF8FB; width: 100%; height: 35%; overflow: auto">
-			<div style="border-bottom: 1px #0D3349 dotted;">
-				Destination (Production)
-			</div>
-			<div>
-				<table id="mydest" class="table" style="border: 0px">
+		<div style=" width: 100%; height: 35%;">
+			
+			<div style="width: 100%;float:left; height:20%;">
+                            
+				<table style="width:100%;">
 					<thead>
-						<tr style="font-size: 0.875em;border: 0px">
-							<td style="width:65%; min-width:55%;"><b>Name of Item</b></td>
-							<td style="width:10%; min-width:10%;"><b>Qty</b></td>
-							<td style="width:10%; min-width:10%;"><b>Units</b></td>
-							<td style="width:10%; min-width:10%;"><b>Rate</b></td>
-							<td style="width:15%; min-width:15%;"><b>Amount</b></td>
-						</tr>
+                                            <tr style="font-size: 0.875em;background-color: #E9F2F5;">
+                                                <td colspan="7" align="center"><b>Destination (Production)</b></td>
+                                            </tr>
+                                            <tr style="font-size: 0.875em;border: 0px;background-color: #C8DBE2;">
+                                                    <td class="tab_col_5"></td>
+                                                    <td class="tab_col_50"><b>Name of Item</b></td>
+                                                    <td class="tab_col_7"><b>Qty</b></td>
+                                                    <td class="tab_col_10"><b>Units</b></td>
+                                                    <td class="tab_col_7"><b>Rate</b></td>
+                                                    <td class="tab_col_10"><b>Amount</b></td>
+                                                    <td class="tab_col_10"></td>
+                                            </tr>
 					</thead>
 				</table>
 			</div>
+			
+			<div style="overflow: none;float:left; width:100%; margin-top: 10px;">
+				<table id="mydest" class="table" style="border: 0px">
+					
+				</table>
+			</div>
+			
+			<div style="height:20%;float:left; margin-bottom: 5px; width:100%;">
+                            <table style="width:100%;">
+                                <thead>
+                                    <tr style="font-size: 0.875em;border: 0px">
+                                        <td class="tab_col_5"></td>
+                                        <td align="center" class="tab_col_50">
+                                            <table style="width:70%">
+                                                <tr>
+                                                    <td><a href="#" onclick="addRow('mydest')" class="btn btn-primary btn-sm"> + Add Row</a></td>
+                                                    <td><a href="#" onclick="deleteSelectedRows('mydest')" class="btn btn-primary btn-sm"> X Delete Rows</a></td>
+                                                    <td><a href="#" onclick="endListAndDisplayTotal('mydest')" class="btn btn-primary btn-sm"> End List</a></td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                        <td align="center" class="tab_col_7">
+                                            <span id="mydestfinal_qty" class="badge badge-info">0</span>
+                                        </td>
+                                        <td class="tab_col_10"></td>
+                                        <td class="tab_col_7"></td>
+                                        <td align="center" class="tab_col_10">
+                                            <span id="mydestfinal_amt" class="badge badge-info">0.00</span>
+                                        </td>
+                                        <td class="tab_col_10"></td>
+                                    </tr>
+                                </thead>
+                            </table>
+                            
+                            
+                        </div>  
 		</div>
 		
 		</div>
@@ -189,11 +195,11 @@ echo $callbackvalue;*/
 					</div>   
 				<!--/div-->
 			</div>
-
+<br style="clear:both" />
 	  	</div>
-	  	
+	  	<br style="clear:both" />
 		</div>
-		
+		<br style="clear:both" />
 	</div>
 	<!--script type="text/javascript" src="test.js"></script-->
 		<script>
@@ -209,8 +215,10 @@ echo $callbackvalue;*/
                         //alert("hi");
                         
 			addRow("mysource");
+                        addRow("mydest");
 			
 			function addRow(tableID) {
+                            
                                 //console.log(tableID);
 				var table = document.getElementById(tableID);
 				var rowCount = table.rows.length;
@@ -236,21 +244,18 @@ echo $callbackvalue;*/
 				var row = table.insertRow(rowCount);
 	
 				var cell1 = row.insertCell(0);
-				cell1.className="tab_col_5";
+				cell1.className="tab_col_4";
 				var element1 = document.createElement("input");
 				element1.type = "checkbox";
 				element1.name=tableID +"chkbox[]";
 				element1.className="form-control";
 				cell1.appendChild(element1);
-	
+                                
 				var cell2 = row.insertCell(1);
-				cell2.className="tab_col_50";
-				var element2 = document.createElement("input");
-				element2.type = "text";
-				element2.name=tableID +"name";
-				element2.className="form-control";
+				var element2 = createSelectBox(tableID,"name","");
 				cell2.appendChild(element2);
-	
+                                
+                                
 				var cell3 = row.insertCell(2);
 				cell3.className="tab_col_7";
 				var element3 = document.createElement("input");
@@ -291,8 +296,36 @@ echo $callbackvalue;*/
                                 
                                 edit_flag = true;
                            	window.current_rowID = rowCount + 1;
+                               // event.preventDefault();
 
 			}
+                        
+                        function createSelectBox(tableID,elementName,selectedValue)
+                        {
+                                var element = document.createElement("select");
+                                element.name=tableID + elementName;
+                            
+                                var option;
+                                var options=["Paper","Color Pencil","Note Book"];
+                                /* we are going to add two options */
+                                /* create options elements */
+                                
+                                for(var i=0; i<options.length; i++)
+                                {
+                                    option = document.createElement("option");
+                                    option.setAttribute("value", options[i]);
+                                    option.innerHTML = options[i];
+                                    if(options[i] === selectedValue)
+                                    {
+                                        option.selected = true;
+                                    }
+                                    element.appendChild(option);
+                                }
+                                
+				element.className="form-control";
+                                
+                                return element;
+                        }
                         
                         function calcTotal(tableID)
                         {
@@ -317,8 +350,8 @@ echo $callbackvalue;*/
                                 final_amt = final_amt + parseFloat(amt[i].innerText);
                             }
                             
-                            document.getElementById(tableID +"final_qty").innerText = final_qty;
-                            document.getElementById(tableID +"final_amt").innerText = final_amt;
+                            document.getElementById(tableID +"final_qty").innerText = final_qty.toLocaleString("en-IN") +" Nos.";
+                            document.getElementById(tableID +"final_amt").innerText = "Rs. "+ final_amt.toLocaleString("en-IN");
                             
                         }
 			
@@ -356,7 +389,7 @@ echo $callbackvalue;*/
 			
 			function tbToLbl(tableID,rowID,action)
 			{
-                            alert("inside tbtolbl fn");
+                            //alert("inside tbtolbl fn");
 				var table = document.getElementById(tableID);
 				
 				var txt_name = document.getElementsByName(tableID +'name')[rowID-1].value;
@@ -364,7 +397,7 @@ echo $callbackvalue;*/
 				var txt_units = document.getElementsByName(tableID +'units')[rowID-1].value;
 				var txt_rate = document.getElementsByName(tableID +'rate')[rowID-1].value;
 				var txt_amt = document.getElementsByName(tableID +'amt')[rowID-1].value;
-				alert(txt_name);
+				//alert(txt_name);
                         
                                 //if ((isNaN(txt_qty) === false) && (isNaN(txt_rate) === false) && (isNaN(txt_amt) === false))
 				//{
@@ -414,7 +447,8 @@ echo $callbackvalue;*/
 				var txt_rate = document.getElementsByName(tableID+'rate')[rowID-1].innerText;
 				var txt_amt = document.getElementsByName(tableID+'amt')[rowID-1].innerText;
 				//alert(txt_name);
-				table.rows[rowID-1].cells[1].innerHTML="<input type='text' name='"+tableID+"name' class='form-control' value='"+txt_name+"'/>";
+                                table.rows[rowID-1].cells[1].innerHTML= "";
+				table.rows[rowID-1].cells[1].appendChild(createSelectBox(tableID,"name",txt_name));
 				table.rows[rowID-1].cells[2].innerHTML="<input type='text' name='"+tableID+"qty' onkeypress='return validate(event,this,\"int\");' class='form-control' value='"+txt_qty+"'/>";
 				table.rows[rowID-1].cells[3].innerHTML="<input type='text' name='"+tableID+"units' class='form-control' value='"+txt_units+"'/>";
 				table.rows[rowID-1].cells[4].innerHTML="<input type='text' name='"+tableID+"rate' onkeypress='return validate(event,this,\"dec\");' class='form-control' value='"+txt_rate+"'/>";
@@ -422,6 +456,17 @@ echo $callbackvalue;*/
 				table.rows[rowID-1].cells[6].innerHTML="<input type='image' src='img/tick.png' height='20px' width='20px' onclick='doneEdit(\""+tableID+"\",\""+rowID+"\")' alt='Done Editing' title='Done Editing'>&nbsp<input type='image' src='img/cross.png' height='20px' width='20px' onclick='cancelEdit(\""+tableID+"\",\""+rowID+"\")' alt='Cancel Editing' title='Cancel Editing'>";
                                 window.edit_flag = true;
                                 window.current_rowID = rowID;
+                        }
+                        
+                        function getNames(tableID)
+                        {
+                            var name = document.getElementsByName(tableID +"name");
+                            var fname="";
+                            for(var i=0 ; i<name.length ; i++)
+                            {
+                                fname = fname + " , " +name[i].innerText;
+                            }
+                            alert(fname);
                         }
                         
                         function doneEdit(tableID,rowID)
@@ -438,14 +483,14 @@ echo $callbackvalue;*/
 			
 			function editRow(tableID,rowID)
 			{
-                                alert("edit rowid : "+window.current_rowID);
-                                alert("edit flag : "+window.edit_flag);
+                                //alert("edit rowid : "+window.current_rowID);
+                                //alert("edit flag : "+window.edit_flag);
         
                                 var table = document.getElementById(tableID);
                                 var rowCount = table.rows.length;
                                 
-                                alert("row count : "+rowCount);
-                                alert("row id : "+rowID);
+                                //alert("row count : "+rowCount);
+                                //alert("row id : "+rowID);
                                 if((window.current_rowID === rowCount))
                                 {
                                     //alert("inside if");
