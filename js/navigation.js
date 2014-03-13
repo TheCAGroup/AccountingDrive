1286d4302a021de28471e4679378a7bf3eadbd4c
yamini <yamini@localhost.localdomain>
2014-03-13 12:58:55 +0000

    Login functionality Added

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,7 @@ function openLicenseCreationDialog()
 
 function gotogroups()
 {
-   	$("#maindiv").load(home+"/Masters/Groups/group.html");
+   	$("#contentdiv").load(home+"/Masters/Groups/group.html");
 }
 
 function openCreateGroupDialog()
@@ -20,31 +20,31 @@ function openCreateCompanyDialog()
 
 function gotoselectcompany()
 {
-   	$("#maindiv").load(home+"/company/select_company.html");
+   	$("#contentdiv").load(home+"/company/select_company.html");
 }
 
 function gotoupdatecompany(selcomp)
 {
-	$("#maindiv").load(home+"/company/create_company.html?ID="+selcomp+"&ISEDIT=TRUE");
+	$("#contentdiv").load(home+"/company/create_company.html?ID="+selcomp+"&ISEDIT=TRUE");
 }
 function gotodisplaycompany(selcomp)
 {
-	$("#maindiv").load(home+"/company/create_company.html?ID="+selcomp);
+	$("#contentdiv").load(home+"/company/create_company.html?ID="+selcomp);
 }   
 function gotoHomePage()
 {
-   	$("#maindiv").load(home+"/License/license.html");
+   	$("#contentdiv").load(home+"/License/license.html");
 }
    
 function gotocreatestockcategory()
 {
-	$("#maindiv").load(home+"/stockcategory/create_stockcategory.html");
+	$("#contentdiv").load(home+"/stockcategory/create_stockcategory.html");
 }
 function gotovoucherentry()
 {
-	$("#maindiv").load(home+"/voucher_entry/voucherentry.html");
+	$("#contentdiv").load(home+"/voucher_entry/voucherentry.html");
 }
 function gotostockjournal()
 {
-	$("#maindiv").load(home+"/voucher/stock_journal.html");
+	$("#contentdiv").load(home+"/voucher/stock_journal.html");
 }
